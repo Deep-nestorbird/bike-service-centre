52bcc28f9c3f8a6bdcc72398d0b237632aa87695
Deep-nestorbird <codingadda007@gmail.com>
2024-06-06 14:59:57 +0530

    final

---
+++ bike_service_centre/bike_service_centre/doctype/service_report/service_report.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Deep Prakash Srivastava and Aman Agnihotri and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Service Report", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ bike_service_centre/bike_service_centre/doctype/service_type/service_type.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Deep Prakash Srivastava and Aman Agnihotri and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class ServiceType(Document):
+	pass
